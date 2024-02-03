c7682d02798e60a070308631af26ced9d6322de8
Mahender Kumar <mksb1947@gmail.com>
2024-02-04 01:30:58 +0530

    working

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,8 @@
-import 'dart:html';
 
 import 'package:chat_app/pages/chat_page.dart';
 import 'package:chat_app/services/auth/auth_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
