fab9a78f4a9e8bad80e288d3d884d2e7b669fd51
Mahender Kumar <mksb1947@gmail.com>
2024-02-04 00:11:58 +0530

    .

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,10 @@
+import 'dart:html';
+
+import 'package:chat_app/pages/chat_page.dart';
 import 'package:chat_app/services/auth/auth_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -10,6 +16,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   void signOut() {
     final authService = Provider.of<AuthService>(context, listen: false);
     authService.signOut();
@@ -20,8 +27,52 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Homepage'),
-        actions: [IconButton(onPressed: signOut, icon: const Icon(Icons.logout))],
+        actions: [
+          IconButton(onPressed: signOut, icon: const Icon(Icons.logout))
+        ],
       ),
+      body: _buildUserList(),
     );
+  }
+
+  Widget _buildUserList() {
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('users').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('eroorr');
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Text('loading');
+          }
+          return ListView(
+            children: snapshot.data!.docs
+                .map<Widget>(
+                  (doc) => _buildUserListItme(doc),
+                )
+                .toList(),
+          );
+        });
+  }
+
+  Widget _buildUserListItme(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data()! as Map<String, dynamic>;
+    if (_auth.currentUser!.email != data['email']) {
+      
+      return ListTile(
+        title: Text(data['email']),
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => ChatPage(
+                        receiverUserEmail: data['email'],
+                        receiverUserId: data['uid'],
+                      )));
+        },
+      );
+    } else {
+      return Container();
+    }
   }
 }
